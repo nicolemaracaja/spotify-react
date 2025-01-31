d5c16536cc5ab0877a80af695afeebaf59383e5d
nicolemaracaja <nicolebrito.maracaja@gmail.com>
2025-01-31 18:01:42 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Reset/Reset.css';
+import './App.css';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <Header></Header>
+  );
+}
+
+export default App;
